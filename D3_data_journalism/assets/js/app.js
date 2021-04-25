e5b60e2c101ad788a1e36d41475eb94ef377539a
Jade <jadeve.jimenez@yahoo.com>
2021-04-24 21:47:45 -0700

    help with dot label

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -11,7 +11,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -19,13 +19,13 @@ var svg = d3.select(".chart")
 var chartGroup = svg.append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv('data/data.csv').then(function(healthData){
+d3.csv('assets/data/data.csv').then(function(healthData){
     healthData.forEach(function(data){
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
     });
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(healthData, d => d.poverty)])
+      .domain([0, d3.max(healthData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -42,17 +42,29 @@ d3.csv('data/data.csv').then(function(healthData){
     chartGroup.append("g")
       .call(leftAxis);
 
-    var circlesGroup = chartGroup.selectAll("scatter")
+    var circlesGroup = chartGroup.selectAll("circle")
       .data(healthData)
       .enter()
-      .append("scatter")
+      .append("circle")
       .attr("cx", d => xLinearScale(d.poverty))
       .attr("cy", d => yLinearScale(d.healthcare))
       .attr("r", "15")
       .attr("fill", "pink")
       .attr("opacity", ".5");
+      
+    var bubbleLabel = circlesGroup.selectAll("text")
+      .data(healthData)
+      .enter()
+      .append("text")
+      .attr('x',function(d){return d.poverty})
+      .attr('y', function(d){return d.healthcare})
+      .attr('text-anchor','middle')
+      .text(function(d){return d.abbr;})
+      .style("fill","black")
+      .style("font-family", "Helvetica Neue, Helvetica, Arial, san-serif")
+      .style("font-size", "5px");
 
-      chartGroup.append("text")
+    chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
